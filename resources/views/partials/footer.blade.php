7aa821b4a6625ae2536e840925c4a00d5da04d92
JCasaisCar <jesuscasacarrillo@gmail.com>
2025-02-17 14:27:04 +0100

    arreglamos lo del footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,9 +3,9 @@
     <div class="container text-center">
         <p>&copy; 2025 MesaYa - Todos los derechos reservados.</p>
         <ul class="list-inline">
+            <li class="list-inline-item"><a href="{{ url('/home') }}">Incicio</a></li>
             <li class="list-inline-item"><a href="{{ url('/nosotros') }}">Sobre Nosotros</a></li>
             <li class="list-inline-item"><a href="{{ url('/contacto') }}">Contacto</a></li>
-            <li class="list-inline-item"><a href="{{ url('/reservas') }}">Reservas</a></li>
         </ul>
         <p>
             <a href="#" class="me-2"><i class="bi bi-facebook"></i></a>
