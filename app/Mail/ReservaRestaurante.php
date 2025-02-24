3073e306e9dd5f0928ee4af20ec6820af8a37d60
JCasaisCar <jesuscasacarrillo@gmail.com>
2025-02-24 21:07:58 +0100

    hacemos que ponga la informacion del cliente en el correo enviado al restaurante

--- app/Mail/ReservaRestaurante.php
+++ app/Mail/ReservaRestaurante.php
@@ -6,16 +6,19 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\ReservaConfirmada;
+use App\Models\User;
 
 class ReservaRestaurante extends Mailable
 {
     use Queueable, SerializesModels;
 
     public $reserva;
+    public $cliente;
 
     public function __construct(ReservaConfirmada $reserva)
     {
         $this->reserva = $reserva;
+        $this->cliente = User::find($reserva->user_id); // Obtener datos del cliente
     }
 
     public function build()
@@ -24,6 +27,7 @@ class ReservaRestaurante extends Mailable
                     ->view('emails.reserva-restaurante')
                     ->with([
                         'reserva' => $this->reserva,
+                        'cliente' => $this->cliente, // Pasamos el cliente a la vista
                     ]);
     }
 }
