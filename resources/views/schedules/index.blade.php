f2918f5de76446f845f206eb38d7fb1635885d88
JCasaisCar <jesuscasacarrillo@gmail.com>
2025-02-26 11:19:00 +0100

    quitamos script innecesario

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -12,22 +12,3 @@
 </div>
 
 @endsection
-
-@section('scripts')
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const selectElement = document.getElementById('unavailable_hours');
-            const message = document.querySelector('.form-text');
-
-            selectElement.addEventListener('change', function () {
-                // Mostrar las horas seleccionadas
-                const selectedHours = Array.from(selectElement.selectedOptions).map(option => option.value);
-                if (selectedHours.length > 0) {
-                    message.textContent = `Has seleccionado ${selectedHours.length} hora(s): ${selectedHours.join(', ')}`;
-                } else {
-                    message.textContent = 'Mantén presionada la tecla Ctrl o Cmd para seleccionar múltiples horas.';
-                }
-            });
-        });
-    </script>
-@endsection
